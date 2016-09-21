82ce390f996fcbee5c5b42e44e796478f0b56697
HEROGWP <pp820819@gmail.com>
2016-09-21 19:44:52 +0800

    Add basic CRUD feature

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module RailsPratice
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    #台北時區設定
+    #config.time_zone = "Taipei"
   end
 end
