76289771397af2263423b234b78cc26ce2cbc13e
HEROGWP <pp820819@gmail.com>
2016-09-30 00:56:35 +0800

    event _form can new or edit category use select and radio_button

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -63,6 +63,6 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:name, :description)
+    params.require(:event).permit(:name, :description, :category_id)
   end
 end
