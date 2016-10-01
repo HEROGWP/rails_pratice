ea283505273cfebee1cd9387e43d452facf261f2
HEROGWP <pp820819@gmail.com>
2016-10-01 17:49:16 +0800

    Authorization user

---
+++ app/controllers/admin/events_controller.rb
@@ -0,0 +1,18 @@
+class Admin::EventsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :check_admin
+	layout "admin"
+
+	def index
+		@events = Event.all
+	end
+
+	protected
+
+
+	def check_admin
+		unless current_user.admin?
+			raise ActiveRecord::RecordNotFound
+		end
+	end
+end
