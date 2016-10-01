ca7a6f6511e41a46a0d316109d3cc58a50e7bfe5
HEROGWP <pp820819@gmail.com>
2016-10-01 11:38:38 +0800

    Multiple update or delete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -57,12 +57,26 @@ class EventsController < ApplicationController
     redirect_to :action => :index
   end
 
+
+
+  def bulk_update
+    ids = Array( params[:ids] )
+    events = ids.map{|i| Event.find_by_id(i) }.compact
+ 
+    if params[:commit] == "Delete"
+      events.each{|e| e.destroy }
+    else
+      events.each{|e| e.update(:status => "Pulished")}
+    end
+
+    redirect_to events_path
+  end
   private
   def set_event
     @event = Event.find(params[:id])
   end
 
   def event_params
-    params.require(:event).permit(:name, :description, :category_id, :group_ids => [])
+    params.require(:event).permit(:name, :description, :category_id, :status, :group_ids => [], :ids => [] )
   end
 end
