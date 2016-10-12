fe2ae325e1d890edc31534dea0f0081f5299726a
HEROGWP <pp820819@gmail.com>
2016-10-12 16:02:19 +0800

    add Facebook login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011084206) do
+ActiveRecord::Schema.define(version: 20161012070158) do
 
   create_table "attendees", force: :cascade do |t|
     t.string   "name"
@@ -99,7 +99,10 @@ ActiveRecord::Schema.define(version: 20161011084206) do
     t.string   "first_name"
     t.string   "last_name"
     t.string   "role"
+    t.string   "fb_uid"
+    t.string   "fb_token"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["fb_uid"], name: "index_users_on_fb_uid"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
